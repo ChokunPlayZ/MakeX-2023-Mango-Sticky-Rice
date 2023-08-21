5d93ad9cbb802a2016346c3b41b16facbf680e3c
Chokun Rojanapron <chokun.swc555@gmail.com>
2023-08-22 06:16:52 +0700

    completed gamepad intelsense

--- mbuild/gamepad.py
+++ mbuild/gamepad.py
@@ -10,3 +10,27 @@ def get_joystick(joystick:str):
     ```
     """
 
+def s_key_pressed(button:str):
+    """
+    Get the status of the button on the makeblock joystick
+    ```
+    {
+        "R1": "Right R1",
+        "R2": "Right R2",
+        "L1": "Left L1",
+        "L2": "Left L2",
+        "N1": "Number 1",
+        "N2": "Number 2",
+        "N3": "Number 3",
+        "N4": "Number 4",
+        "Up": "Arrow key up",
+        "Down": "Arrow key down",
+        "Left": "Arrow key left",
+        "Right": "Arrow key right",
+        "+": "Plus Key",
+        “≡“: "Menu key",
+        "L_Thumb": "left joystick pressed",
+        "R_Thumb": "right joystick pressed"
+    }
+    ```
+    """
